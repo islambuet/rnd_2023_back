76ced795a376d38073290206c89b7fb9425cc636
Md. Shaiful Islam <islambuet@gmail.com>
2022-10-11 23:22:08 +0600

    Users Task started

---
+++ app/Http/Controllers/users/UsersController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Controllers\users;
+
+use App\Http\Controllers\RootController;
+
+use Illuminate\Http\JsonResponse;
+
+
+class UsersController extends RootController
+{
+    public function initialize(): JsonResponse
+    {
+        if ($this->permissions->action_0 == 1){
+            $response= [];
+            $response['error'] = '';
+            $response['permissions'] = $this->permissions;
+            return response()->json($response);
+        }
+        else{
+            return response()->json(['error'=>'ACCESS_DENIED','messages'=>__('You do not have access on this page')]);
+        }
+    }
+}
+
