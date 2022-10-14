e4a6d088c7a247f7c769928d254386d5c04885dd
Md. Shaiful Islam <islambuet@gmail.com>
2022-10-15 01:45:30 +0600

    Update UsersController.php

--- app/Http/Controllers/users/UsersController.php
+++ app/Http/Controllers/users/UsersController.php
@@ -70,7 +70,8 @@ class UsersController extends RootController
         if ($this->permissions->action_0 == 1){
             /** @noinspection DuplicatedCode */
             $query=DB::table(TABLE_USERS.' as users');
-            $query->select('users.id','users.username','users.user_group_id','users.name','users.email','users.mobile_no','users.ordering','users.status','users.created_at');
+            $query->select('users.id','users.username','users.user_group_id','users.name','users.email','users.mobile_no',
+                'users.ordering','users.status','users.max_logged_browser','users.mobile_authentication_off_end','users.created_at');
             $query->join(TABLE_USER_GROUPS.' as user_groups', 'user_groups.id', '=', 'users.user_group_id');
             $query->addSelect('user_groups.name as user_group_name');
             $query->join(TABLE_USER_TYPES.' as user_types', 'user_types.id', '=', 'users.user_type_id');
@@ -79,6 +80,9 @@ class UsersController extends RootController
             $result = $query->first();
             if(!$result){
                 return response()->json(['error'=>'ITEM_NOT_FOUND','messages'=>__('Invalid Id '.$itemId)]);
+            }
+            if($result->mobile_authentication_off_end< Carbon::now()){
+                $result->mobile_authentication_off_end=null;
             }
             $response=[];
             $response['error'] = '';
