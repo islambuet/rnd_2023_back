f592e2b24eeac06cb8b066e4e067755acd1b2053
Md. Shaiful Islam <islambuet@gmail.com>
2022-10-20 05:22:51 +0600

    Update route.api.php

--- app/Http/Controllers/setup/route.api.php
+++ app/Http/Controllers/setup/route.api.php
@@ -32,6 +32,17 @@ Route::middleware('logged-user')->group(function()use ($url,$controllerClass){
     Route::post($url.'/save-item', [$controllerClass, 'saveItem']);
 });
 
+$url='setup/seasons';
+$controllerClass= Controllers\setup\SeasonsController::class;
+/** @noinspection DuplicatedCode */
+Route::middleware('logged-user')->group(function()use ($url,$controllerClass){
+    Route::match(['GET','POST'],$url.'/initialize', [$controllerClass, 'initialize']);
+    Route::match(['GET','POST'],$url.'/get-items', [$controllerClass, 'getItems']);
+    Route::match(['GET','POST'],$url.'/get-item/{itemId}', [$controllerClass, 'getItem']);
+    Route::post($url.'/save-item', [$controllerClass, 'saveItem']);
+});
+
+
 $url='setup/crops';
 $controllerClass= Controllers\setup\CropsController::class;
 /** @noinspection DuplicatedCode */
@@ -41,5 +52,4 @@ Route::middleware('logged-user')->group(function()use ($url,$controllerClass){
     Route::match(['GET','POST'],$url.'/get-item/{itemId}', [$controllerClass, 'getItem']);
     Route::post($url.'/save-item', [$controllerClass, 'saveItem']);
 });
-
 
