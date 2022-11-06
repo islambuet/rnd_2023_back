493972e98ae833c93648705be7c773e5a52d787a
Md. Shaiful Islam <islambuet@gmail.com>
2022-11-06 21:10:49 +0600

    Variety Task included

--- app/Http/Controllers/setup/route.api.php
+++ app/Http/Controllers/setup/route.api.php
@@ -62,4 +62,13 @@ Route::middleware('logged-user')->group(function()use ($url,$controllerClass){
     Route::match(['GET','POST'],$url.'/get-item/{itemId}', [$controllerClass, 'getItem']);
     Route::post($url.'/save-item', [$controllerClass, 'saveItem']);
 });
+$url='setup/varieties';
+$controllerClass= Controllers\setup\VarietiesController::class;
+/** @noinspection DuplicatedCode */
+Route::middleware('logged-user')->group(function()use ($url,$controllerClass){
+    Route::match(['GET','POST'],$url.'/initialize', [$controllerClass, 'initialize']);
+    Route::match(['GET','POST'],$url.'/get-items', [$controllerClass, 'getItems']);
+    Route::match(['GET','POST'],$url.'/get-item/{itemId}', [$controllerClass, 'getItem']);
+    Route::post($url.'/save-item', [$controllerClass, 'saveItem']);
+});
 
