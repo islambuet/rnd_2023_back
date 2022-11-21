caa4d06c5aa155b889360aecb68152f51c8b246d
Md. Shaiful Islam <islambuet@gmail.com>
2022-11-21 22:00:42 +0600

    delivery task included

--- app/Http/Controllers/variety_configuration/route.api.php
+++ app/Http/Controllers/variety_configuration/route.api.php
@@ -11,3 +11,13 @@ Route::middleware('logged-user')->group(function()use ($url,$controllerClass){
 //    Route::match(['GET','POST'],$url.'/{cropId}/{year}/get-item/{varietyId}', [$controllerClass, 'getItem']);
     Route::post($url.'/{cropId}/{year}/save-item', [$controllerClass, 'saveItem']);
 });
+
+$url='variety-configuration/delivery';
+$controllerClass= Controllers\variety_configuration\VarietyDeliveryController::class;
+/** @noinspection DuplicatedCode */
+Route::middleware('logged-user')->group(function()use ($url,$controllerClass){
+    Route::match(['GET','POST'],$url.'/initialize', [$controllerClass, 'initialize']);
+//    Route::match(['GET','POST'],$url.'/get-items', [$controllerClass, 'getItems']);
+//    Route::match(['GET','POST'],$url.'/get-item/{itemId}', [$controllerClass, 'getItem']);
+//    Route::post($url.'/save-item', [$controllerClass, 'saveItem']);
+});
